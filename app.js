74a112f49ee9618f19669449e51cd715a2fe9e9f
najwa2222 <karrouchinajwa@gmail.com>
2025-04-25 00:30:09 +0100

    db connection

--- app.js
+++ app.js
@@ -33,10 +33,10 @@ app.use(express.urlencoded({ extended: true }));
 
 // — DB & Init —
 const pool = mysql.createPool({
-  host:     process.env.DB_HOST,
-  user:     process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host:     process.env.MYSQL_HOST,
+  user:     process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DB,
   waitForConnections: true,
   connectionLimit: 10
 });
