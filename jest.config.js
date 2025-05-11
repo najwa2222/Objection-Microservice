2c8b98b83d30d71be01d47bd3c499680e656f269
najwa2222 <karrouchinajwa@gmail.com>
2025-05-11 01:55:19 +0100

    objection ready

--- jest.config.js
+++ jest.config.js
@@ -4,9 +4,10 @@ export default {
     '^mysql2/promise$': '<rootDir>/__mocks__/mysql2/promise.js',
     '^express-mysql-session$': '<rootDir>/__mocks__/express-mysql-session.js',
   },
-  setupFilesAfterEnv: ['<rootDir>/jest/setup.js'],
+  // Either remove this line or ensure the file exists
+  // setupFilesAfterEnv: ['<rootDir>/jest/setup.js'], 
   testMatch: ['**/__tests__/**/*.test.js'],
-  resolver: '<rootDir>/jest-resolver.cjs', // Updated to use the CJS adapter
+  resolver: '<rootDir>/jest-resolver.cjs', // Use the CommonJS adapter
   transform: {},
   testTimeout: 30000,
   clearMocks: true,
